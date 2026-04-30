8a6271a8576c811bde63568e5677b8b9d4c6c15c
Muntasir <alimuntasir2001@gmail.com>
2026-04-30 23:49:27 +0600

    Practice 1

---
+++ src/primitive.ts
@@ -0,0 +1,21 @@
+// js - Ts
+
+// String,Number,Boolian,Undefined,Null,Symbol,BigInt
+//Ts: never, unknown, any, void
+//String -> 
+
+let userName : string = "Muntasir345"
+
+userName = "John"
+
+let userId = 12345
+
+userId = 123
+
+// userName.toFixed()
+userId.toFixed()
+
+
+let isAdmin : boolean = false
+
+isAdmin = true
