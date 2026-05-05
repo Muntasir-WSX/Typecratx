acd4a145466fa17b8656b398bee5552b374345d8
Muntasir <alimuntasir2001@gmail.com>
2026-05-05 23:31:49 +0600

    Class & OOP Practice

---
+++ src/class.ts
@@ -0,0 +1,21 @@
+//oop - class -obj
+
+class Animal {
+    name: string;
+    species: string;
+    sound: string;
+
+    constructor ( name: string,species: string, sound: string) {
+        this.name = name;
+        this.species = species;
+        this.sound = sound;
+    }
+}
+
+const dog = new Animal("dog", "mammal", "woof");
+const cat = new Animal("cat", "mammal", "meow");
+
+console.log(dog);
+console.log(cat);
+
+
