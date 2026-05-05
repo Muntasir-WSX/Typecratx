874b7568489e5d5be6af8a6cc5ff3a88fae529c6
Muntasir <alimuntasir2001@gmail.com>
2026-05-05 23:33:08 +0600

    Class & OOP Practice 2

--- src/class.ts
+++ src/class.ts
@@ -1,17 +1,22 @@
 //oop - class -obj
 
-class Animal {
-    name: string;
-    species: string;
-    sound: string;
+// class Animal {
+//     name: string;
+//     species: string;
+//     sound: string;
 
-    constructor ( name: string,species: string, sound: string) {
-        this.name = name;
-        this.species = species;
-        this.sound = sound;
+//     constructor ( name: string,species: string, sound: string) {
+//         this.name = name;
+//         this.species = species;
+//         this.sound = sound;
+//     }
+// }
+
+//parameter properties
+class Animal {
+    constructor ( public name: string, public species: string, public sound: string) {
     }
 }
-
 const dog = new Animal("dog", "mammal", "woof");
 const cat = new Animal("cat", "mammal", "meow");
 
