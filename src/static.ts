d44ff57db0285e48f0e13c81ad22869afe1bea4c
Muntasir <alimuntasir2001@gmail.com>
2026-05-06 23:17:39 +0600

    Static Practice

---
+++ src/static.ts
@@ -0,0 +1,24 @@
+// static
+
+class Counter{
+  static  count: number = 0;
+    increment (){
+        return Counter.count = Counter.count+1;
+    }
+    decerement(){
+        return Counter.count = Counter.count-1;
+    }
+
+}
+
+const instance1 = new Counter();
+
+console.log(instance1.increment());
+
+
+
+const instance2 = new Counter();
+
+console.log(instance2.increment());  
+console.log(instance2.increment());
+console.log(instance2.decerement());
